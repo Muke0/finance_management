91c0b7f338b44f46e3fdc983b43278ac55973158
cbpantianya <cbpantianya@163.com>
2023-05-14 18:48:44 +0800

    feat(web): add new website base on vue3

---
+++ finance-web-vue3/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-menu default-active="1" class="sidebar">
+    <div class="title">
+      财务管理系统
+    </div>
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon>
+          <Menu />
+        </el-icon>
+        <span class="menu">工资管理</span>
+      </template>
+      <el-menu-item index="1-1" class="menu-item">数据上报</el-menu-item>
+      <el-menu-item index="1-2" class="menu-item">数据审核</el-menu-item>
+      <el-menu-item index="1-3" class="menu-item">报表生成</el-menu-item>
+    </el-sub-menu>
+  </el-menu>
+  <div class="container">
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import axios from 'axios';
+import { ElMessage } from 'element-plus'
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      username: "获取中..."
+    };
+  },
+  created() { },
+  mounted() { 
+    if (localStorage.getItem('token')) {
+      // 获取用户信息
+      axios.get('/user/info',{
+        headers: {
+          Authorization: localStorage.getItem('token')
+        }
+      }).then(res => {
+        if (res.data.user_name) {
+          this.username = res.data.user_name;
+        } else {
+          ElMessage({
+            message: res.data.message,
+            type: 'error',
+          })
+        }
+      })
+    }
+  },
+};
+</script>
+
+<style scoped>
+.sidebar {
+  width: 200px;
+  overflow: auto;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin: 20px 0;
+}
+
+.menu {
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.menu-item {
+  font-size: 16px;
+}
+
+.container {
+  width: 100%;
+}
+
+
+</style>
---
+++ finance-web-vue3/src/views/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <headbar></headbar>
+    <div>
+        <p>欢迎访问</p>
+        <p>学校工资管理系统</p>
+    </div>
+</template>
+
+<script>
+import headbar from '../components/headbar.vue'
+export default {
+    name: 'Index',
+    components: {
+        headbar
+    },
+    data() {
+        return {
+            username: '获取中...'
+        }
+    },
+}
+
+</script>
+
+<style scoped>
+.header {
+    height: 60px;
+    background-color: #ffffff;
+    border-bottom: 1px solid #ccc;
+    box-shadow: 0 0 10px #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+
+.avatar-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+}
+
+.avatar-btn span {
+    margin-left: 10px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #555;
+}
+</style>
