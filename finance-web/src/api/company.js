3c824aebb0f3e3fed9d40c8e09ebb275c7f5f3c2
610759768@qq.com <610759768@qq.com>
2023-05-27 16:11:40 +0800

    rm admin web

--- finance-web/src/api/company.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-import da from 'element-ui/src/locale/lang/da'
-
-export function getInfo() {
-  return request({
-    url: '/company/info',
-    method: 'get'
-  })
-}
